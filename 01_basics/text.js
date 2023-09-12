a2d6241044e03904020405a335fc31112dbdaa2d
MRshubhambaluni <144840913+MRshubhambaluni@users.noreply.github.com>
2023-09-12 20:13:00 +0000

    done again

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,14 @@
-console.log("Shubham");
+/*
+basic or var ,let and const
+*/
+let accountId = 12345;
+var accontstate = "jaipur";
+const accountdeatail = "none";
+let account;
+accontstate = "ranipur"
+accountId = "6312658  "
+    // console.log(accountId);
+    // console.log(accontstate);
+    // console.log(accountdeatail);
+    // console.log(account);
+    console.table([accountId, accontstate, accountdeatail, account]);
---
+++ basicjs/test.js
@@ -0,0 +1,10 @@
+let name = "shubham";
+let age = 23;
+let indian = "true";
+let state;
+let state1 = null
+console.log( typeof name);
+console.log(typeof age);
+console.log(typeof indian);
+console.log(typeof state);
+console.log(typeof state1);
