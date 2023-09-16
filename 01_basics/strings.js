6da15c59fbbdd7ac53838549e112610ff58f46b4
MRshubhambaluni <144840913+MRshubhambaluni@users.noreply.github.com>
2023-09-16 12:08:49 +0000

    done

---
+++ 01_basics/strings.js
@@ -0,0 +1,35 @@
+let Name = "shubham";
+ let age = 50;
+// console.log(`my name is ${Name} and my age is ${age}.`)
+    // const Name2 = new String('hello wKrld')
+// console.log(Name2.length);
+// console.log(Name2.at);
+    //  console.log(Name2.big());
+        // console.log(Name2.charAt(6));
+//console.log(Name2.toUpperCase());
+    // console.log(Name2.toLowerCase());
+    //   console.log(Name2.split());
+    // let vocab = ('abcdefghijklmnoprrstuvwxyz')
+    // console.log(vocab.length);
+        // let x = new String("John");
+        // let y = new String("John");
+        // console.log(x==y);
+        // let a = "5";
+        // let b = 5;
+        // console.log(a==b);
+
+  let clas = new String(`hello friends- i am ${Name} .i am from- kotdwar-my age is ${age}`) 
+//   console.log(clas);
+//   console.log(clas.length);
+  const all = (`  hello-world   `)
+  const oll = (`my-name -is shubham`)
+//   console.log(all);
+//   console.log(all.trim());
+//    console.log(clas.substring(0, 6));
+//   console.log(oll.slice(0, 7));
+//   console.log(oll.includes("shubham"));
+//   console.log(oll.replace("shubham", "mohit"));
+const more = (`hello word`);
+console.log(more.length);    
+         
+        
