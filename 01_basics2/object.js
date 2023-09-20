33779fcd8fd4a65d3f83bb0265db915055b1bcb8
MRshubhambaluni <144840913+MRshubhambaluni@users.noreply.github.com>
2023-09-20 04:32:24 +0000

    done objct

---
+++ 01_basics2/object.js
@@ -0,0 +1,37 @@
+let jsUser = {
+    name: "shubham",
+    age: 24,
+    email: "shubhambaluni",
+    locaion: "delhi"
+}
+// console.log(jsUser.name)
+// console.log(jsUser["name"])
+// jsUser.email=("chuguhdchehfurehfdh")
+// // Object.freeze(jsUser)
+// jsUser.email =("kkkkkkkk")
+// console.log(jsUser["name"])
+// console.log(jsUser["name"])
+// console.log(jsUser["email"])
+/////////////////////////////////////////////////////////////////////////////////
+let name2 = "baluni"
+let age2  = 34
+function greeting(username){
+    console.log(`good morning everyone i am ${name2}`)
+
+}
+// console.log(greeting())
+
+
+
+function addFourumers(num1, num2, num3, num4){
+ return num1+num2+num3+num4;
+}
+// console.log(addFourumers(2,3,2,3))
+
+
+function addnumer(num1,num2){
+  result = num1+num2
+  return result
+}
+console.log(`there is a two numers add ${addnumer(2,4)}`)
+console.log(result);
